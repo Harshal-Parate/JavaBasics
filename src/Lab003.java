35b7f1852ec46be56453a82b81665456e64b0d5d
Harshal-Parate <Harshalparate1@gmail.com>
2023-12-24 12:33:37 +0530

    test commit

---
+++ src/Lab003.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        String a = "Harshal";
+        String b = "Parate";
+        System.out.println(a+" "+b);
+    }
+}
